72d099d61ab961bf7c0c60de950d1136d77d863b
Trần Thái Dương <135450063+td0710@users.noreply.github.com>
2025-07-02 19:46:57 +0700

    Time: 1197 ms (8.74%), Space: 64.9 MB (15.54%) - LeetHub

---
+++ 0315-count-of-smaller-numbers-after-self/0315-count-of-smaller-numbers-after-self.java
@@ -0,0 +1,26 @@
+class Solution {
+    public List<Integer> countSmaller(int[] nums) {
+        List<Integer> li = new ArrayList<>() ;
+        List<Integer> sort = new ArrayList<>() ;
+
+        for(int i = nums.length-1;i>=0;i--) {
+            int ind = insert(sort,nums[i]);
+            li.add(ind);
+            sort.add(ind,nums[i]) ; 
+        }
+
+        Collections.reverse(li) ; 
+        return li;
+    }
+
+    private int insert(List<Integer> arr, int num) {
+        int l = 0; 
+        int r = arr.size() - 1;
+        while(l<=r) {
+            int mid = l+(r-l)/2;
+            if(arr.get(mid) <num) l = mid+1; 
+            else r = mid-1; 
+        }
+        return l; 
+    }
+}
