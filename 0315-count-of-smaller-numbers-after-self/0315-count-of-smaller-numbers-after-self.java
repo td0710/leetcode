ff7d7bec6cd56dd1b10bace1ffc4002d458c7cc8
Trần Thái Dương <135450063+td0710@users.noreply.github.com>
2025-07-08 12:25:57 +0700

    Time: 1212 ms (5.24%), Space: 63 MB (35.85%) - LeetHub

--- 0315-count-of-smaller-numbers-after-self/0315-count-of-smaller-numbers-after-self.java
+++ 0315-count-of-smaller-numbers-after-self/0315-count-of-smaller-numbers-after-self.java
@@ -15,11 +15,11 @@ class Solution {
 
     private int insert(List<Integer> arr, int num) {
         int l = 0; 
-        int r = arr.size() - 1;
-        while(l <= r) {
-            int mid = l+(r - l + 1) / 2;
-            if(arr.get(mid) < num) l = mid + 1; 
-            else r = mid-1; 
+        int r = arr.size();
+        while(l < r) {
+            int mid = (r + l) / 2;
+            if(arr.get(mid) >= num) r = mid ; 
+            else l = mid + 1; 
         }
         return l; 
     }
